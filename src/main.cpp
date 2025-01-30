8bb04c05add29089953994d6ee3a5dccc2943603
Jeffrey Abreu <jeffreyabreu66@gmail.com>
2025-01-30 12:07:51 -0500

    added vertex attributes

--- src/main.cpp
+++ src/main.cpp
@@ -16,17 +16,6 @@ int main()
         return -1;
     }
 
-   /* // If a GLFW function fails, we report an error to the error callback
-
-    void error_callback(int error, const char* description)
-    {
-        fprintf(stderr, "Error: %s\n", description);
-    }*/
-
-    // Function to set the error callback is one of the few GLFW functions that can be called BEFORE initialization
-
-   // glfwSetErrorCallback(error_callback);
-
     //Create the window using a single call to glfwCreateWindow (width, height, title, monitor(for fullscreen or NULL for windowed), share (window whose context to share resorurces with, NULL to not share))
     // Note that GLFWwindow is the TYPE here.
     window = glfwCreateWindow(640, 480, "Physics_Sim", NULL, NULL);
@@ -42,12 +31,39 @@ int main()
     // This context will REMAIN current until we make another context current or the window owning the context is destroyed
     glfwMakeContextCurrent(window);
 
+
+    if(!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        return -1;
+    }
+
+    float positions[6] = {
+       -0.5f, -0.5f,
+        0.0f,  0.5f,
+        0.5f, -0.5f
+    };
+
+    unsigned int buffer;
+    // Generates a buffer and gives us back an ID
+    glGenBuffers(1, &buffer);
+
+    // Select that buffer using bindBuffer
+    //GL_ARRAY_BUFFER tells us that this is an array
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+
+    // Creates and initializes a buffer object's data store
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
     // Loop until we decide to close the window
     while(!glfwWindowShouldClose(window))
     {
         // render screen
-       // glClear(GL_COLOR_BUFFER_BIT); --> This line was causing issues
+        glClear(GL_COLOR_BUFFER_BIT); 
 
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         // swap front and back buffers
         glfwSwapBuffers(window);
 
@@ -59,9 +75,8 @@ int main()
 
 
 
-   /* // Initialize all OpenGL pointers required for this program to interact w/ OpenGL API
+    // Initialize all OpenGL pointers required for this program to interact w/ OpenGL API
     // We need this line or we wouldn't be able to call OpenGL functions beyond the basic ones, such as funcs for creating shaders, buffers, etc...
-    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);*/
-
+   
     return 0;
 }
